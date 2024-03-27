8fb6640860ed2693a7ec5f9f34ee2c0c53e53659
Paja777 <pavle.rvovic95@gmail.com>
2024-03-27 10:07:12 +0100

    Middleware for frontend routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose");
+const path = require('path');
 const productRoutes = require("./routes/products");
 const userRoutes = require("./routes/users");
 const dataRoutes = require('./routes/data')
@@ -20,9 +21,17 @@ app.use(cors({
     credentials: true 
 }));
 
+// Serve static files from the build directory
+app.use(express.static(path.join(__dirname, 'dist')));
+
 app.use("/product", productRoutes);
 app.use("/user", userRoutes);
 app.use("/data", dataRoutes);
+
+// Serve the index.html file for all other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 mongoose 
   .connect(process.env.MONGO_URI, {
